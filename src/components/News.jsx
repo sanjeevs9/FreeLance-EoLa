d06863b3a6405d02250c1abd6ac16afc65b1556d
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-06-14 16:28:54 +0530

    fix

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,7 +7,7 @@ const img3="https://eola.qodeinteractive.com/wp-content/uploads/2018/05/blog-1-i
 export default function News(){
     return(
         <>
-    <div className="flex justify-center pl-12 pr-12 lg:pl-10 lg:pr-10  md:pl-0 md:pr-0">
+    <div className="flex justify-center pl-12 pr-12 md:pl-10 md:pr-10  ">
         <div className="flex flex-col justify-center items-center pt-20 md:w-[80rem]  ">
             <div className="pb-10">
                 <div className="text-[#f5989d] font-cormorant-gara text-center text-xl font-medium italic">
