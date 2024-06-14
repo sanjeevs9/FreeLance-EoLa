0d591252b152e51bf4aa0f31b877520abb7f5edc
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-06-14 15:26:18 +0530

    fix

--- src/components/CarouselContent.jsx
+++ src/components/CarouselContent.jsx
@@ -5,8 +5,8 @@ export default function CarouselContent({content,writer}){
         <div className="flex items-center justify-center ">
         <div className="flex flex-col justify-center items-center w-10/12">
             <div className="font-work-sans text-6xl">“</div>
-            <div className="pb-10 text-center font-cormorant-gara italic text-[#a6a4a4] text-[22px]  ">{content}</div>
-            <div className="font-cormorant-uni font-bold text-lg">{writer}</div>
+            <div className="pb-10 text-center font-cormorant-gara italic text-[#a6a4a4] text-[18px] sm:text-[22px]  ">{content}</div>
+            <div className="font-cormorant-uni font-bold  sm:text-lg">{writer}</div>
         </div>
         </div>
         </>
