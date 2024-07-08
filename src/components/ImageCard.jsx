48dc952dbd4a9fceee37ce9de672364b649fe383
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-07-08 22:31:47 +0530

    fix

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -35,7 +35,7 @@ const container = {
       scale: 1,
       y:0,
      transition:{
-        delay:0.9,
+        delay:0.7,
         duration:0.5,
      }
     }
@@ -48,7 +48,7 @@ const container = {
       scale: 1,
       y:0,
      transition:{
-        delay:1.1,
+        delay:0.7,
         duration:0.5,
      }
     }
@@ -66,18 +66,20 @@ export default function ImageCard({image,paragraph,title,text}){
                 <img src={image} className="h-screen w-full object-cover"/>
                     <motion.div className=" " variants={container} initial="hidden" animate={inView ? "visible" : "hidden"}>
                     <div className="z-1 flex flex-col bg-white absolute justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  w-[19rem] 2xl:w-[47rem]  xl:w-[44rem] lg:w-[42rem] md:w-[41rem] sm:w-[40rem]   h-[12.5rem]   2xl:h-[21rem]  xl:h-[20rem] lg:h-[19rem] md:h-[19rem] sm:h-[18rem] ">
-                        <motion.div variants={p} initial="hidden" animate={inView ? "visible" : "hidden"}>
+                        <motion.div className="items-center text-center" variants={p} initial="hidden" animate={inView ? "visible" : "hidden"}>
                          <p className="text-sm md:text-xl   font-cormorant-gara text-[#f5989d]  italic">{paragraph}</p>
-                         </motion.div>
-                         <motion.div variants={t} initial="hidden" animate={inView ? "visible" : "hidden"}>
+                        
+                       
                          <h2 className="  text-4xl md:text-6xl sm:text-5xl   font-cormorant-uni font-light pb-10">{title}</h2>
-                         </motion.div>
-                         <motion.div variants={txt} initial="hidden" animate={inView ? "visible" : "hidden"}>
-                         <div className=" bg-gradient-to-tl from-[#f5989d] via-[#cae2cd] to-[#f5989d] p-[1.7px]  items-center justify-center">
+                        
+                        <div className="flex justify-center ">
+                         <div className="w-fit  bg-gradient-to-tl from-[#f5989d] via-[#cae2cd] to-[#f5989d] p-[1.7px]  items-center justify-center ">
                             
-                            <button className="bg-white  font-work-sans text-sm pt-4 pb-4 pl-11 pr-11 sm:pt-5 sm:pb-5 sm:pl-12 sm:pr-12 ">{text}</button>
+                            <button className=" bg-white  font-work-sans text-sm pt-4 pb-4 pl-11 pr-11 sm:pt-5 sm:pb-5 sm:pl-12 sm:pr-12 ">{text}</button>
+                         </div>
                          </div>
                          </motion.div>
+                        
                     </div>
                     </motion.div>
             </div>
